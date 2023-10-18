5d3fa0ca3bbc768f9057023984e0e6b55a3c0bd6
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-18 15:35:37 -0400

    Initial Course navigation implementation

--- src/Kanbas/Courses/CourseNavigation/index.js
+++ src/Kanbas/Courses/CourseNavigation/index.js
@@ -1,23 +1,28 @@
-import { Link, useParams, useLocation } from "react-router-dom";
+import { Link, useParams, useLocation } from 'react-router-dom';
+import { FaEyeSlash } from 'react-icons/fa';
+import './index.css';
 
 function CourseNavigation() {
-    const links = ["Home", "Modules", "Assignments", "Grades"];
+    const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes", "Grades", "People", "Panopto Video", "Discussions", "Announcements", "Pages",
+        "Files", "Rubrics", "Outcomes", "Collaborations", "Syllabus", "Progress Reports (EAB Navigate)", "Settings",];
+
     const { courseId } = useParams();
     const { pathname } = useLocation();
+
     return (
-        <div className="list-group" style={{ width: 150 }}>
+        <ul className="course-nav">
             {links.map((link, index) => (
-                <Link
-                    key={index}
-                    to={`/Kanbas/Courses/${courseId}/${link}`}
-                    className={`list-group-item ${pathname.includes(link) && "active"}`}>
-                    {link}
-                </Link>
+                <li key={index} className={pathname.includes(link) ? 'course-nav-selected' : ''}>
+                    <Link to={`/Kanbas/Courses/${courseId}/${link.toLowerCase()}/screen.html`}>{link}</Link>
+                    {link === 'Discussions' || link === 'Announcements' || link === 'Pages' || link === 'Files' || link === 'Rubrics' || link === 'Outcomes' || link === 'Collaborations' || link === 'Syllabus' ? (
+                        <i className="fa-regular fa-eye-slash fa-flip-horizontal">
+                            <FaEyeSlash className="fa-2x" />
+                        </i>
+                    ) : null}
+                </li>
             ))}
-        </div>
+        </ul>
     );
 }
 
-
 export default CourseNavigation;
-
