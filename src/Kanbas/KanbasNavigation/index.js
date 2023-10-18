b813b6285e0f28b016d46f222a85098717e2b4f0
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-18 15:35:29 -0400

    Initial Kanbas navigation implementation

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,18 +1,52 @@
 import { Link, useLocation } from "react-router-dom";
-function KanbasNavigation() {
-    const links = ["Account", "Dashboard", "Courses", "Calendar"];
+import "./index.css";
+import { FaCircleUser, FaGaugeHigh, FaBook, FaCalendarDays, FaEnvelopeOpenText, FaClock, FaNetworkWired, FaRightFromBracket, FaCircleQuestion } from 'react-icons/fa6';
+
+const KanbasNavigation = () => {
+    const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
     const { pathname } = useLocation();
+
+    const getIconForLink = (link) => {
+        switch (link) {
+            case "Account":
+                return <FaCircleUser className="kb-nav-icon fa-2xl account-icon" />;
+            case "Dashboard":
+                return <FaGaugeHigh className="kb-nav-icon fa-flip-horizontal fa-2xl" />;
+            case "Courses":
+                return <FaBook className="kb-nav-icon fa-2xl" />;
+            case "Calendar":
+                return <FaCalendarDays className="kb-nav-icon fa-2xl" />;
+            case "Inbox":
+                return <FaEnvelopeOpenText className="kb-nav-icon fa-2xl" />;
+            case "History":
+                return <FaClock className="kb-nav-icon fa-2xl" />;
+            case "Studio":
+                return <FaNetworkWired className="kb-nav-icon fa-2xl" />;
+            case "Commons":
+                return <FaRightFromBracket className="kb-nav-icon fa-2xl" />;
+            case "Help":
+                return <FaCircleQuestion className="kb-nav-icon fa-2xl" />;
+            default:
+                return null;
+        }
+    };
+
     return (
-        <div className="list-group" style={{ width: 150 }}>
-            {links.map((link, index) => (
-                <Link
-                    key={index}
-                    to={`/Kanbas/${link}`}
-                    className={`list-group-item ${pathname.includes(link) && "active"}`}>
-                    {link}
+        <ul className="kanbas-nav min-vh-100">
+            <li>
+                <Link to="/Kanbas/Dashboard">
+                    <img src="/images/nu_logo.png" alt="NU Logo" className="nu-logo" />
                 </Link>
+            </li>
+            {links.map((link, index) => (
+                <li key={index} className={pathname.includes(link) ? "kanbas-nav-selected" : ""}>
+                    <Link to={`/Kanbas/${link}`} className="kanbas-nav-link">
+                        {getIconForLink(link)}<br />
+                        <span className="kanbas-nav-text">{link}</span>
+                    </Link>
+                </li>
             ))}
-        </div>
+        </ul>
     );
-}
+};
 export default KanbasNavigation;
