04b69878a63d4784fdf97e28080990f17099f20c
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-17 21:37:43 -0400

    Implement Labs for A3

---
+++ src/Labs/a3/JsonStringify.js
@@ -0,0 +1,11 @@
+const JsonStringify = () => {
+    const squares = [1, 4, 16, 25, 36];
+
+    return (
+        <div>
+            <h2>JSON Stringify</h2>
+            squares = {JSON.stringify(squares)}<br />
+        </div>
+    )
+}
+export default JsonStringify
