a16bc95139a2b9ce3314c287d9b8209c85a5a650
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-18 14:04:58 -0400

    Add initial A3 Kanbas placeholder code

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,11 +1,21 @@
-import Nav from "../Nav";
+import KanbasNavigation from "./KanbasNavigation";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Courses from "./Courses";
+
+import "./index.css";
 
 function Kanbas() {
     return (
-        <div className="container">
-            <Nav />
-            <h1>Kanbas</h1>
+        <div className="d-flex">
+            <KanbasNavigation />
+            <Routes>
+                <Route path="/" element={<Navigate to="Dashboard" />} />
+                <Route path="Account" element={<h1>Account</h1>} />
+                <Route path="Dashboard" element={<Dashboard />} />
+                <Route path="Courses/:courseId/*" element={<Courses />} />
+            </Routes>
         </div>
     );
 }
-export default Kanbas
+export default Kanbas;
