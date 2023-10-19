4f5c7b308c5f950aa55aeac1c219eaaf21fea728
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-19 02:28:23 -0400

    Add blank account page

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -2,6 +2,7 @@ import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
+import Account from "./Account";
 import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js";
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js";
 
@@ -13,7 +14,7 @@ function Kanbas() {
             <KanbasNavigation />
             <Routes>
                 <Route path="/" element={<Navigate to="Dashboard" />} />
-                <Route path="Account" element={<h1>Account</h1>} />
+                <Route path="Account" element={<Account />} />
                 <Route path="Dashboard" element={<Dashboard />} />
                 <Route path="Courses/:courseId/*" element={<Courses />} />
             </Routes>
