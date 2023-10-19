202cdec07f08a16e8e368fe752c66115ee62f2af
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-19 18:10:24 -0400

    Fix selection for elements with space

--- src/Kanbas/Courses/CourseNavigation/index.js
+++ src/Kanbas/Courses/CourseNavigation/index.js
@@ -2,9 +2,12 @@ import { Link, useParams, useLocation } from 'react-router-dom';
 import { FaEyeSlash } from 'react-icons/fa';
 import './index.css';
 
-function CourseNavigation() {
-    const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes", "Grades", "People", "Panopto Video", "Discussions", "Announcements", "Pages",
-        "Files", "Rubrics", "Outcomes", "Collaborations", "Syllabus", "Progress Reports (EAB Navigate)", "Settings",];
+const CourseNavigation = () => {
+    const links = [
+        "Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes",
+        "Grades", "People", "Panopto Video", "Discussions", "Announcements", "Pages",
+        "Files", "Rubrics", "Outcomes", "Collaborations", "Syllabus", "Progress Reports (EAB Navigate)", "Settings"
+    ];
 
     const { courseId } = useParams();
     const { pathname } = useLocation();
@@ -14,8 +17,8 @@ function CourseNavigation() {
             <p className="cnav-small-text text-body-secondary">202410_1 Fall 2023 Semester</p>
             <ul className="course-nav">
                 {links.map((link, index) => (
-                    <li key={index} className={pathname.includes(link) ? 'course-nav-selected' : ''}>
-                        <Link to={`/Kanbas/Courses/${courseId}/${link}`}>{link}</Link>
+                    <li key={index} className={pathname.includes(encodeURIComponent(link)) ? 'course-nav-selected' : ''}>
+                        <Link to={`/Kanbas/Courses/${courseId}/${encodeURIComponent(link)}`}>{link}</Link>
                         {link === 'Discussions' || link === 'Announcements' || link === 'Pages' || link === 'Files' || link === 'Rubrics' || link === 'Outcomes' || link === 'Collaborations' || link === 'Syllabus' ? (
                             <FaEyeSlash className="ms-1 fa-2x" />
                         ) : null}
