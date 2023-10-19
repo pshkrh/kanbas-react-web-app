01a73338a2742b93d94e49a3ffdeef19b2a28e09
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-19 17:15:45 -0400

    Fix conditional styling

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
-import { FaGripVertical, FaCaretDown, FaPlus, FaEllipsisVertical, FaCircleCheck } from "react-icons/fa6";
+import { FaGripVertical, FaCaretDown, FaPlus, FaEllipsisVertical, FaCircleCheck, FaLink, FaArrowUpRightFromSquare } from "react-icons/fa6";
 
 function ModuleList() {
     const { courseId } = useParams();
@@ -15,7 +15,7 @@ function ModuleList() {
                             <FaGripVertical className="me-2" />
                             <FaPlus className="me-2" />
                             <div className="module-title flex-grow-1">{module.title}</div>
-                            <FaCircleCheck className="me-2" style={{ color: 'green' }} />
+                            <FaCircleCheck className="me-2" style={{ color: "green" }} />
                             <FaCaretDown className="me-2" />
                             <FaPlus className="me-3" />
                             <FaEllipsisVertical />
@@ -26,17 +26,42 @@ function ModuleList() {
                             <li className="list-group-item module-border">
                                 <div className="d-flex justify-content-start align-items-center">
                                     <FaGripVertical className="me-2" />
-                                    <div className="module-subtitle flex-grow-1">{submodule.subtitle}</div>
-                                    <FaCircleCheck className="me-3" style={{ color: 'green' }} />
+                                    <div className="module-subtitle flex-grow-1">
+                                        {submodule.subtitle.toUpperCase()}
+                                    </div>
+                                    <FaCircleCheck className="me-3" style={{ color: "green" }} />
                                     <FaEllipsisVertical />
                                 </div>
                             </li>
-                            {submodule.items.map((item, itemIndex) => (
-                                <li className="list-group-item module-border" key={itemIndex}>
+                            {submodule.items.length > 0 && submodule.items.map((item, itemIndex) => (
+                                <li
+                                    className={`list-group-item module-border ${submodule.subtitle === "Slides" ? "slides-item" : ""
+                                        }`}
+                                    key={itemIndex}
+                                >
                                     <div className="d-flex justify-content-start align-items-center">
                                         <FaGripVertical className="me-2" />
-                                        <div className="module-item flex-grow-1">{item}</div>
-                                        <FaCircleCheck className="me-3" style={{ color: 'green' }} />
+                                        {submodule.subtitle === "Slides" && (
+                                            <FaLink style={{ color: "green" }} />
+                                        )}
+                                        {submodule.subtitle === "Slides" ? (
+                                            <>
+                                                <div className="module-link flex-grow-1">
+                                                    <a
+                                                        className="link"
+                                                        href={item.url}
+                                                    >
+                                                        <span className="me-1">{item.title}</span>
+                                                        <FaArrowUpRightFromSquare className="link" />
+                                                    </a>
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <span className="module-item flex-grow-1">
+                                                {item.title}
+                                            </span>
+                                        )}
+                                        <FaCircleCheck className="me-3" style={{ color: "green" }} />
                                         <FaEllipsisVertical />
                                     </div>
                                 </li>
