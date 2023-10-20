a869465edd070b84cfed0561f3a5e778e08ace7b
Pushkar Kurhekar <pshkrh@gmail.com>
2023-10-19 20:41:04 -0400

    Implement grades screen

--- src/Kanbas/Courses/Grades/index.js
+++ src/Kanbas/Courses/Grades/index.js
@@ -1,35 +1,81 @@
-import db from "../../Database";
-import { useParams } from "react-router-dom";
-import GradesTopBar from "./GradesTopBar";
-import "./index.css"
+import React, { useState } from 'react';
+import db from '../../Database';
+import { useParams } from 'react-router-dom';
+import GradesTopBar from './GradesTopBar';
+import './index.css';
 
 function Grades() {
     const { courseId } = useParams();
     const assignments = db.assignments.filter((assignment) => assignment.course === courseId);
     const enrollments = db.enrollments.filter((enrollment) => enrollment.course === courseId);
+
+    const [editableGrades, setEditableGrades] = useState({});
+
+    const handleGradeChange = (enrollmentId, assignmentId, value) => {
+        setEditableGrades((prevGrades) => ({
+            ...prevGrades,
+            [`${enrollmentId}-${assignmentId}`]: value,
+        }));
+    };
+
     return (
         <div>
             <GradesTopBar />
             <div className="table-responsive">
-                <table className="table">
-                    <thead>
-                        <th>Student Name</th>
-                        {assignments.map((assignment) => (<th>{assignment.title}</th>))}
+                <table className="table table-striped table-bordered border-secondary text-center">
+                    <thead className="table-secondary">
+                        <tr>
+                            <th>Student Name</th>
+                            {assignments.map((assignment) => (
+                                <th key={assignment._id}>
+                                    {assignment.title}
+                                    <br />
+                                    Out of {assignment.points}
+                                </th>
+                            ))}
+                        </tr>
                     </thead>
                     <tbody>
                         {enrollments.map((enrollment) => {
                             const user = db.users.find((user) => user._id === enrollment.user);
                             return (
-                                <tr>
-                                    <td>{user.firstName} {user.lastName}</td>
+                                <tr key={enrollment.user}>
+                                    <td>
+                                        {user.firstName} {user.lastName}
+                                    </td>
                                     {assignments.map((assignment) => {
                                         const grade = db.grades.find(
-                                            (grade) => grade.student === enrollment.user && grade.assignment === assignment._id);
-                                        return (<td>{grade?.grade || ""}</td>);
+                                            (grade) =>
+                                                grade.student === enrollment.user && grade.assignment === assignment._id
+                                        );
+                                        const isEditable = true; // You can add conditions here to determine if it's editable
+                                        const gradeKey = `${enrollment.user}-${assignment._id}`;
+                                        const value = editableGrades[gradeKey] !== undefined ? editableGrades[gradeKey] : grade?.grade || '';
+
+                                        return (
+                                            <td key={assignment._id}>
+                                                {isEditable ? (
+                                                    <div className="form-group">
+                                                        <input
+                                                            type="text"
+                                                            className="form-control"
+                                                            value={value}
+                                                            onChange={(e) => handleGradeChange(enrollment.user, assignment._id, e.target.value)}
+                                                        />
+                                                    </div>
+                                                ) : (
+                                                    grade?.grade != null ? `${grade.grade}%` : ''
+                                                )}
+                                            </td>
+                                        );
                                     })}
-                                </tr>);
+                                </tr>
+                            );
                         })}
-                    </tbody></table>
-            </div></div>);
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 export default Grades;
