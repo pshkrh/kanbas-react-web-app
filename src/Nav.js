91f74bc4af424f6f517540e36a40e41b3df56903
Pushkar Kurhekar <pshkrh@gmail.com>
2023-11-28 20:33:36 -0500

    Rename tab to A6

--- src/Nav.js
+++ src/Nav.js
@@ -14,7 +14,7 @@ function Nav() {
             <Link to="/Kanbas"
                 className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}>Kanbas</Link>
             <Link to="/project/home"
-                className={`nav-link ${pathname.includes("project") ? "active" : ""}`}>Project</Link>
+                className={`nav-link ${pathname.includes("project") ? "active" : ""}`}>A6</Link>
         </nav>
     );
 }
